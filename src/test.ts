103664d688fc88df2d470cda56a979d468c1d417
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-04-20 22:41:26 +0300

    add exemple test

---
+++ src/test.ts
@@ -0,0 +1,11 @@
+type User = 'admin' | 'role';
+
+const userName: User = 'admin';
+
+console.log(userName);
+
+// function Button(props) {
+//   return <button>{props.text}</button>;
+// }
+// <Button text="Click me" />
+// <Button text="Delete" />
